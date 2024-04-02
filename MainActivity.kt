7b8e77fb3599cbe6bd7347bb85acfc51f9109bd4
irungu101 <158286774+irungu101@users.noreply.github.com>
2024-04-02 16:51:21 +0300

    Add files via upload

--- MainActivity.kt
+++ MainActivity.kt
@@ -11,9 +11,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -21,12 +25,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -76,6 +85,7 @@ fun HelloWorld(name: String){
        Spacer(modifier = Modifier.height(20.dp))
        TextFieldLocationComponent()
        Spacer(modifier = Modifier.height(20.dp))
+       PasswordTextField()
        Button(onClick = {},
            modifier = Modifier
                .fillMaxWidth()
@@ -93,6 +103,8 @@ fun HelloWorld(name: String){
            colors = ButtonDefaults.buttonColors(Color.DarkGray))
        {
            Text(text = "Register Here")}
+
+
 
 
 //       Text(text = "Hey I am a software Developer/ Android Developer",
@@ -162,6 +174,44 @@ fun TextFieldLocationComponent(){
             Text(text = "Please Enter your Location")
         })
 
+
+}
+@Composable
+fun PasswordTextField(){
+    var password by rememberSaveable {
+        mutableStateOf("")
+    }
+    var passwordVisibility by remember {
+        mutableStateOf(false)
+    }
+    val icon = if (passwordVisibility)
+        painterResource(id = R.drawable.design_ic_visibility)
+    else
+        painterResource(id = R.drawable.design_ic_visibility_off)
+    OutlinedTextField(
+        modifier = Modifier.fillMaxWidth(),
+        value = password,
+        onValueChange = {
+            password = it
+        },
+        placeholder = { Text(text = "Password") },
+        label = { Text(text = "Password") },
+        trailingIcon = {
+            IconButton(onClick = {
+                passwordVisibility = !passwordVisibility
+            }) {
+                Icon(
+                    painter = icon,
+                    contentDescription = "Visibility Icon"
+                )
+            }
+        },
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Password
+        ),
+        visualTransformation = if (passwordVisibility) VisualTransformation.None
+        else PasswordVisualTransformation()
+    )
 
 }
 
