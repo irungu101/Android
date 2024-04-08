d8c3320c79c403bba0879146fa4663bc07070422
irungu101 <158286774+irungu101@users.noreply.github.com>
2024-04-08 16:41:04 +0300

    Add files via upload

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,217 +1,149 @@
-package com.alex.myfirstandroidproject
+package com.naomi.jetpackcompose
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.text.style.TextAlign
+
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.alex.myfirstandroidproject.ui.theme.MyFirstAndroidProjectTheme
+import com.naomi.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyFirstAndroidProjectTheme {
+            JetpackComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                   HelloWorld("Robert")
+                    Column {
+
+
+
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth(0.5f)
+                                .padding(20.dp)
+
+                        ){
+                            ImageCard(painter = painterResource(id = R.drawable.card), contentDescription = "mycard", title = "This is a nature picture")
+                        }
+                        Spacer(modifier = Modifier.height(20.dp))
+                        Spacer(modifier = Modifier.height(20.dp))
+
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth(0.5f)
+                                .padding(20.dp)
+
+                        ){
+                            ImageCard2(painter = painterResource(id = R.drawable.nature), contentDescription = "mycard2", title = "This is my nature picture")
+                        }
+
+                    }
+
                 }
             }
         }
     }
 }
 
-
 @Composable
-fun HelloWorld(name: String){
-   Column {
-
-       Text(text = "Hey $name! Welcome Back",
-           fontSize = 15.sp,
-           color = Color.Black,
-           fontFamily = FontFamily.SansSerif,
-           fontStyle = FontStyle.Italic,
-           textAlign = TextAlign.Center,
-           modifier = Modifier
-               .background(Color.Gray)
-               .padding(20.dp)
-               .wrapContentHeight(align = Alignment.CenterVertically)
-
-
-
-       )
-       Spacer(modifier = Modifier.height(20.dp))
-       TextFieldEmailComponent()
-       Spacer(modifier = Modifier.height(20.dp))
-       TextFieldComponent()
-       Spacer(modifier = Modifier.height(20.dp))
-       TextFieldLocationComponent()
-       Spacer(modifier = Modifier.height(20.dp))
-       PasswordTextField()
-       Button(onClick = {},
-           modifier = Modifier
-               .fillMaxWidth()
-               .padding(10.dp),
-           colors = ButtonDefaults.buttonColors(Color.DarkGray))
-       {
-           Text(text = "Login Here")
-       Spacer(modifier = Modifier.height(20.dp))
-
-       }
-       Button(onClick = {},
-           modifier = Modifier
-               .fillMaxWidth()
-               .padding(10.dp),
-           colors = ButtonDefaults.buttonColors(Color.DarkGray))
-       {
-           Text(text = "Register Here")}
+fun ImageCard(painter: Painter, contentDescription:String,title:String){
+    Card(
+        modifier = Modifier.wrapContentWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = CardDefaults.cardElevation(20.dp)
+    ){
+        Box(
+            modifier = Modifier.height(200.dp)
+        )
+        {
+            Image(painter = painter, contentDescription = contentDescription, contentScale = ContentScale.Crop)
+            Box (
+                modifier = Modifier
+                    .matchParentSize()
+                    .padding(15.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(text = title,
+                    color = Color.Black,
+                    fontSize = 15.sp
+                )
+            }
+        }
 
 
+    }
+}
+@Composable
+fun ImageCard2(painter: Painter, contentDescription:String,title:String){
+    Card(
+        modifier = Modifier.wrapContentWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = CardDefaults.cardElevation(20.dp)
+    ){
+        Box(
+            modifier = Modifier.height(200.dp)
+        )
+        {
+            Image(painter = painter, contentDescription = contentDescription, contentScale = ContentScale.Crop)
+            Box (
+                modifier = Modifier
+                    .matchParentSize()
+                    .padding(15.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(text = title,
+                    color = Color.Black,
+                    fontSize = 15.sp
+                )
+            }
+        }
 
 
-//       Text(text = "Hey I am a software Developer/ Android Developer",
-//           fontSize = 23.sp,
-//           color = Color.Blue,
-//           fontFamily = FontFamily.SansSerif,
-//           fontStyle = FontStyle.Italic,
-//           textAlign = TextAlign.Center,
-//           modifier = Modifier.background(Color.Gray)
-//               .padding(20.dp)
-//               .wrapContentHeight(align = Alignment.CenterVertically)
-//
-//
-//
-//
-//       )
-   }
+    }
+}
+
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun ImageCardPreview(){
+    ImageCard(painter = painterResource(id = R.drawable.card), contentDescription = "mycard", title = "This is a nature picture" )
+
 }
 @Preview(showSystemUi = true, showBackground = true)
 @Composable
-fun HelloWorldPreview(){
-    HelloWorld("Robert")
-}
-@Composable
-fun TextFieldComponent(){
-    var text by remember {
-        mutableStateOf(value = "")
-    }
-    TextField(modifier = Modifier.fillMaxWidth(),
-        value = text, onValueChange = {
-        newText -> text = newText
-    },
-    label = {Text(text = "Enter your Name")}, 
-        placeholder = {
-            Text(text = "Please Enter your Name")
-        })
-
+fun ImageCardPreview2(){
+    ImageCard(painter = painterResource(id = R.drawable.nature), contentDescription = "mycard2", title = "This is my nature picture" )
 
 }
-@Composable
-fun TextFieldEmailComponent(){
-    var text by remember {
-        mutableStateOf(value = "")
-    }
-    TextField(modifier = Modifier.fillMaxWidth(),
-        value = text, onValueChange = {
-                newText -> text = newText
-        },
-        label = {Text(text = "Enter your Email")},
-        placeholder = {
-            Text(text = "Please Enter your Email")
-        })
-
-
-}
-@Composable
-fun TextFieldLocationComponent(){
-    var text by remember {
-        mutableStateOf(value = "")
-    }
-    TextField(modifier = Modifier.fillMaxWidth(),
-        value = text, onValueChange = {
-                newText -> text = newText
-        },
-        label = {Text(text = "Enter your Location")},
-        placeholder = {
-            Text(text = "Please Enter your Location")
-        })
-
-
-}
-@Composable
-fun PasswordTextField(){
-    var password by rememberSaveable {
-        mutableStateOf("")
-    }
-    var passwordVisibility by remember {
-        mutableStateOf(false)
-    }
-    val icon = if (passwordVisibility)
-        painterResource(id = R.drawable.design_ic_visibility)
-    else
-        painterResource(id = R.drawable.design_ic_visibility_off)
-    OutlinedTextField(
-        modifier = Modifier.fillMaxWidth(),
-        value = password,
-        onValueChange = {
-            password = it
-        },
-        placeholder = { Text(text = "Password") },
-        label = { Text(text = "Password") },
-        trailingIcon = {
-            IconButton(onClick = {
-                passwordVisibility = !passwordVisibility
-            }) {
-                Icon(
-                    painter = icon,
-                    contentDescription = "Visibility Icon"
-                )
-            }
-        },
-        keyboardOptions = KeyboardOptions(
-            keyboardType = KeyboardType.Password
-        ),
-        visualTransformation = if (passwordVisibility) VisualTransformation.None
-        else PasswordVisualTransformation()
-    )
-
-}
-
